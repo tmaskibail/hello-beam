e1adc00045e14c0816fa11371d2fcbddd4d0cf8c
tmaskibail <thilak.maskibail@gmail.com>
2020-05-01 11:56:24 +0100

    init commit

---
+++ src/main/java/com/maskibail/data/BeamBasics.java
@@ -0,0 +1,13 @@
+package com.maskibail.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class BeamBasics
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/maskibail/data/BeamParDo.java
@@ -0,0 +1,4 @@
+package com.maskibail.data;
+
+public class BeamParDo {
+}
